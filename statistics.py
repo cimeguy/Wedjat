c12d73f6de738f86bb092ca643ee5d1515fd0cd1
Li Gao <39960989+cimeguy@users.noreply.github.com>
2024-02-21 16:41:53 +0800

    Delete statistics.py

--- statistics.py
+++
@@ -1,6 +0,0 @@
-packet_list = ["Packet_2_10", "Packet_3_5","Packet_1_15", "Packet_1_5"]
-
-sorted_list = sorted(packet_list, key=lambda x: (int(x.split('_')[1]), int(x.split('_')[2])))
-
-print(sorted_list)
-
